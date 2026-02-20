45a8458e76f34a9c8edc25c0032595c2b5c3c326
Samuel Rudqvist <samuel@Samuels-MacBook-Air.local>
2026-02-20 09:22:28 -0600

    fix treesitter issue

--- init.lua
+++ init.lua
@@ -1035,7 +1035,9 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    version = false,
     build = ':TSUpdate',
+    main = 'nvim-treesitter.config',
     opts = {
       ensure_installed = { 'bash', 'c', 'html', 'css', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
@@ -1055,7 +1057,7 @@ require('lazy').setup({
       -- Prefer git instead of curl in order to improve connectivity in some environments
       require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter.config').setup(opts)
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
